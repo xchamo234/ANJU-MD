efab44c0ca661edc2d30b04be8e293e35b21bff1
xchamo234 <xchamonirmal9@gmail.com>
2025-03-23 09:01:36 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,8 +40,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~qIlzECbQ#X77K28lAbY-PmPZ9bg5V2WqzUdDGVLrlYhtFjj_kUpA",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~KBFW1Aia#tiik0YhXq6Q1iRhLA8KmTY7C_XlH08uRWnMWlP3R2sA",
 SUDO: process.env.SUDO || ["94743430452","94775512050","94765451617","94768242348"],
 PORT: process.env.PORT || "8000",
-FOOTER: process.env.FOOTER || "> ᴄ.ɴ ᴍᴏᴠɪᴇ ʜᴜʙ_🎬 ʙʏ 𝙭𝘾𝙝𝙖𝙢𝙤!☁️",
+FOOTER: process.env.FOOTER || "> ᴄ.ɴ ᴍᴏᴠɪᴇ ʜᴜʙ ʙʏ 𝙭𝘾𝙝𝙖𝙢𝙤!☁️",
 };
